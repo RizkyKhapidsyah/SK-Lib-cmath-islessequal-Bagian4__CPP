15c767c485ded9925a2371c7ec2a775c50787b78
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2021-09-01 19:39:29 +0700

    Add project files.

---
+++ SK-Lib-cmath-islessequal-Bagian4__CPP/Source.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cmath>
+#include <conio.h>
+
+/*
+	Source by GeeksForGeeks
+	Modified For Learn by RK
+	I.D.E : VS2019
+*/
+
+using namespace std;
+
+int main() {
+	int i = 1;
+
+	while (islessequal(i, 10)) {
+		cout << i << " ";
+		i++;
+	}
+
+	_getch();
+	return 0;
+}
